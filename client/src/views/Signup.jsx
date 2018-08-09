496eb300028700648c4c989ad1fb15015c4baa3f
frank <frankcorkery@gmail.com>
2018-08-09 17:01:16 -0400

    adding bootstrap navabar

--- client/src/views/Signup.jsx
+++ client/src/views/Signup.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 
 class Signup extends React.Component {
   constructor(props) {
-    super(props);
-
+    super(props)
+    this.state = {
+      loggedIn: false,
+      username: '',
+      password: ''
+    }
+    // this.renderUsenameInput = this.renderUsenameInput.bind(this)
   }
+
+  // renderUsenameInput() {
+
+  // }
+
 
   render() {
     return (
-      <div>This is the 'Sigup' component</div>
+      <div>This is the 'Sigup' component
+      <input onChange={ this.renderUsenameInput }>
+      </input>
+      <button></button>
+      </div>
     );
   }
 }
