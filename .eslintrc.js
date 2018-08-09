02199cc78bc71dadb85255214984326be43e650a
parkermuir <parker.muir@gmail.com>
2018-08-09 16:57:01 -0400

    Set up .eslintrc with AirBnB Style Guide

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  rules: {
+    /* Indentation */
+    'no-mixed-spaces-and-tabs': 2,
+    'indent': [2, 2],
+    /* Quotes */
+    'quotes': [1, 'single'],
+
+  }
+};
