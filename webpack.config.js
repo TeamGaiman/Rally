09147b9dc4ea3ca87c351269ae02205da1ecc7b1
parkermuir <parker.muir@gmail.com>
2018-08-09 12:01:41 -0400

    Updated webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,16 @@ const path = require('path');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
-const config = {
-  entry: './index.js',
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
-    path: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    inline: true,
-    port: 8080
+    filename: 'bundle.js',
+    path: DIST_DIR
   },
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
+        test: /\.jsx?/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
@@ -25,5 +21,3 @@ const config = {
     ]
   }
 };
-
-module.exports = config;
