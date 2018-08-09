3b7adfd22d2c6a78cd8fd18c091fe65d78ca4377
parkermuir <parker.muir@gmail.com>
2018-08-09 12:41:55 -0400

    Main component renders on load

---
+++ client/src/views/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>This is the 'NavBar' component</div>
+    );
+  }
+}
+
+
+export default NavBar;
