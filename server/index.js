d8d2a9248109916495f043b1017466eb5a61a767
JKAnderson409 <JKAnderson409@gmail.com>
2018-08-12 18:59:15 -0400

    Initialized postgreSQL db on server launch, moved graphQL files to temp file until they're needed to work with the db/client

--- server/index.js
+++ server/index.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const parser = require('body-parser');
-var graphqlHTTP = require('express-graphql');
-// var { buildSchema } = require('graphql');
-var { schema, root } = require('./schema.js');
+const models = require('../db/models');
+const port = process.env.PORT || 8080;
 
-let app = express();
-
+const app = express();
 app.use(parser.json());
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.use('/graphql', graphqlHTTP({ // config properties used for middleware
-  schema: schema, 
-  rootValue: root,
-  graphiql: true
-}));
-
-app.listen(4000, () => console.log(`Now browse to localhost:4000/graphql`));
-
-
-let port = process.env.PORT || 8080;
-app.listen(port, () => console.log('listening on port: ', port));
+models.sequelize.sync({ force: true })
+  .then(() => {
+    app.listen(port, () => console.log('listening on port: ', port));
+  });
