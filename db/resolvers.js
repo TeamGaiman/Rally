4dba3b9a2b03be5d037c2f5d722f06cd13014a6b
Hack Reactor Student <student@hackreactor.com>
2018-08-14 14:07:40 -0400

    added firebase

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,24 +1,24 @@
-const models = require('./index.js');
+// const models = require('./index.js');
 
-const resolvers = {
-  Query: {
-    getAllUsers: async (_) => {
-      return await models.User.findAll({})
-    },
-    getUser: async (_, {name}) => {
-      return await models.User.findOne({ where: { name }})
-    },
-    checkEmailIsUnique: async(_, {email}) => {
-      return await (models.User.findOne({ where: { email }}))
-       ? false : true
-    }
-  },
-  Mutation: {
-    createUser: async (_, {input}) => {
-      models.User.create(input);
-      return await input;
-    }
-  }
-};
+// const resolvers = {
+//   Query: {
+//     getAllUsers: async (_) => {
+//       return await models.User.findAll({})
+//     },
+//     getUser: async (_, {name}) => {
+//       return await models.User.findOne({ where: { name }})
+//     },
+//     checkEmailIsUnique: async(_, {email}) => {
+//       return await (models.User.findOne({ where: { email }}))
+//        ? false : true
+//     }
+//   },
+//   Mutation: {
+//     createUser: async (_, {input}) => {
+//       models.User.create(input);
+//       return await input;
+//     }
+//   }
+// };
 
-module.exports = resolvers;
+// module.exports = resolvers;
