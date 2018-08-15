8382495c4555267248e8dfcab68350b70edfaff2
JKAnderson409 <JKAnderson409@gmail.com>
2018-08-15 17:59:11 -0400

    Fixed something I missed

--- workers/matchmaking.js
+++ workers/matchmaking.js
@@ -1,9 +1,9 @@
 const eloCalculations = require('../eloCalculations');
 
-const getMatchesByElo = (eloToMatch, users) => {
+const getMatchesByElo = ( eloToMatch, users ) => {
   let validMatches = [];
   users.forEach(user => {
-    if (eloCalculations.calcProbabilityOfWin(eloToMatch, user.elo) >= 0.33) {
+    if ( eloCalculations.calcProbabilityOfWin(eloToMatch, user.elo) >= 0.33 ) {
       validMatches.push(user.name);
     }
   });
