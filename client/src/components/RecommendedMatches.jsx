f9a33c25ff5695468c20e453cfa1129f92615bc7
Edward Kye <edwardkye@me.com>
2018-08-16 11:22:43 -0400

    added table to render all recommended matches

--- client/src/components/RecommendedMatches.jsx
+++ client/src/components/RecommendedMatches.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 
 class RecommendedMatches extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      matches: [
+        {particpantB: 'Acer123', startTime: '2018-08-29 04:00:00', location: 'Central Park'},
+        {particpantB: 'TennisPro', startTime: '2019-08-29 04:00:00', location: 'Bryant Park'},
+      ]
     };
+  }
+
+  getAllMatches() {
+
   }
 
   render() {
     return (
       <div>
-        This is the 'RecommendedMatches' component
-        
+        <h2>Recommended Matches</h2>
+        <Table striped bordered condensed hover>
+          <thead>
+            <tr>
+              <th>User</th>
+              <th>Date</th>
+              <th>Location</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.matches.map(match => (
+              <tr>
+                <td>{match.particpantB}</td>
+                <td>{match.startTime}</td>
+                <td>{match.location}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     );
   }
