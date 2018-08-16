847aadb1dbe8f8f4594d07b841f8f9f722df0072
frank <frankcorkery@gmail.com>
2018-08-16 11:59:05 -0400

    Added button on Profle page to send user to Input Profile Information

---
+++ client/src/components/ProfileInfo.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ProfileInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        This is the 'Profile Info' component
+        
+      </div>
+    );
+  }
+}
+
+
+export default ProfileInfo;
