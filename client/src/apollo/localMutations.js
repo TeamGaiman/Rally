4275d530d021971127cd6151ae9f2af294297232
frank <frankcorkery@gmail.com>
2018-08-17 10:33:48 -0400

    added create user and create match client mutations

--- client/src/apollo/localMutations.js
+++ client/src/apollo/localMutations.js
@@ -1,12 +1,38 @@
 import gql from 'graphql-tag';
 
 // ¡UNTESTED!
-const CREATE_NEW_USER = gql`
-  {
-    createUser($email: String!) {
+const CREATE_USER = gql `
+  mutation ($email: String!) {
+    createUser(input: {
+      email: $email
+    }){
       email
     }
   }
+  `;
+
+
+const CREATE_MATCH = gql `
+  mutation ($participantA: String!, $participantB: String!, $startTime: String!, $location: String!) {
+  createMatch(input: {
+    participantA: $participantA,
+    participantB: $participantB,
+    startTime: $startTime,
+    location: $location
+  }){
+    participantA
+    participantB
+    startTime
+    location
+    
+  }
+}
 `;
+
+
+
 // ¡UNTESTED MUTATION NOT EXPORTED!
-module.exports;
+module.exports = {
+  CREATE_USER,
+  CREATE_MATCH
+};
