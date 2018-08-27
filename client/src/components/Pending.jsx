06c8a9b74fabf1965249263d524bfbbc3d4eaad0
parkermuir <parker.muir@gmail.com>
2018-08-27 11:51:16 -0400

    Stores/renders from state for all pending/completed matches

--- client/src/components/Pending.jsx
+++ client/src/components/Pending.jsx
@@ -1,45 +1,70 @@
 import React from 'react';
 import { Table, Button } from 'react-bootstrap';
+import moment from 'moment';
 
-const Pending = (props) => {
-  return (
-    <div className="matches-container">
-      <h2>Scheduled Matches</h2>
-      <Table striped bordered condensed hover>
-        <thead>
-          <tr>
-            <th>Opponent</th>
-            <th>Date</th>
-            <th>Location</th>
-            <th>Status</th>
-            <th>Winner</th>
-          </tr>
-        </thead>
+class Pending extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      matches: []
+    };
+  }
 
-        <tbody>
-          { props.playerData.pendingMatches.slice( 0, 5 ).map(( challenge ) => {
-            return (
-              <tr className="match-row" key={ challenge.id }>
-                <td>{ challenge.opponent }</td>
-                <td>{ challenge.startTime.split(' GMT')[0] }</td>
-                <td>{ challenge.location }</td>
-                <td>Complete</td>
-                <td>
-                  <Button 
-                    bsStyle="primary" 
-                    onClick={ () => console.log('clicked') }
-                  >
-                    Add Results
-                  </Button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
+  componentDidMount () {
+    console.log(this.props.playerData);
+    let {pendingMatches, completedMatches } = this.props.playerData;
+    let matches = pendingMatches.concat(completedMatches);
 
-      </Table>
-    </div>
-  );
-};
+    this.setState({
+      matches
+    });
+  }
+
+  render () {
+    return (
+      <div className="matches-container">
+        <h2>Scheduled Matches</h2>
+        <Table striped bordered condensed hover>
+        
+          <thead>
+            <tr>
+              <th>Opponent</th>
+              <th>Date</th>
+              <th>Location</th>
+              <th>Status</th>
+              <th>Winner</th>
+            </tr>
+          </thead>
+  
+          <tbody>
+            { this.state.matches.map(( match ) => {
+              return (
+                <tr className="match-row" key={ match.id }>
+                  <td>{ match.opponent }</td>
+                  <td>{ moment( match.startTime ).calendar() }</td>
+                  <td>{ match.location }</td>
+                  <td>Complete</td>
+                  <td>
+                    { match.completed ?
+                      match.winner
+                      :
+                      <Button 
+                        bsStyle="primary" 
+                        onClick={ () => console.log('clicked') }
+                      >
+                        Add Results
+                      </Button>
+                    }
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+  
+        </Table>
+      </div>
+    );
+  }
+}
 
 export default Pending;
