a3083900c885857b602cfbfc173fc839ff133648
JKAnderson409 <JKAnderson409@gmail.com>
2018-08-27 11:48:33 -0400

    Search users by email, username and fullname implemented

--- client/src/components/SearchUsers.jsx
+++ client/src/components/SearchUsers.jsx
@@ -1,46 +1,97 @@
 import React from 'react';
-import Autocomplete from 'react-autocomplete';
+import Autosuggest from 'react-autosuggest';
 import Mutation from 'react-apollo';
 
 import { CREATE_MATCH } from '../apollo/mutations.js';
 
 class SearchUsers extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
     this.state = {
-      textField: '',
-      selectedUser: null
+      placeholder: 'Search for players by email or username',
+      value: '',
+      suggestions: [],
     };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
+    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
   }
 
-  handleBrowseUsers ( user ) {
-    console.log( user );
+  onChange ( event, { newValue, method }) {
     this.setState({
-      selectedUser: user
+      value: newValue
     });
   }
 
-  render() {
+  escapeRegexCharacters (str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
+  
+  getSuggestions (value) {
+    const escapedValue = this.escapeRegexCharacters(value.trim());
+    if (escapedValue === '') {
+      return [];
+    }
+  
+    const regex = new RegExp('^' + escapedValue, 'i');
+  
+    return this.props.allUsers.filter( user => (
+      regex.test ( user.email )
+        || regex.test( user.name )
+        || regex.test( user.fullName )
+    ));
+  }
+  
+  getSuggestionValue (suggestion) {
+    return suggestion.name;
+  }
+  
+  renderSuggestion ( suggestion ) {
     return (
-      <div>
-        { this.props.allUsers
-          ? <Autocomplete
-            getItemValue={ item => this.handleBrowseUsers( item )}
-            items={ this.props.allUsers || ['loading'] }
-            renderItem={(item, isHighlighted) =>
-              <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
-                { item.name }:  { item.email }
-              </div>
-            }
-            value={this.state.textField}
-            onChange={ e => this.state.textField = e.target.value}
-            onSelect={ () => 
-              this.props.handleMatchClick( this.state.selectedUser )
-            }
-          />
-          : null
-        }
-      </div>
+      <span>
+        {(suggestion.email)
+          ? <span><strong>Email: </strong> {suggestion.email}</span>
+          : null}
+        {(suggestion.name)
+          ? <span><strong> Username: </strong> {suggestion.name}</span>
+          : null}
+        {(suggestion.fullName)
+          ? <span><strong> Full Name: </strong> {suggestion.fullName}</span>
+          : null}
+      </span>
+    );
+  }
+
+  onSuggestionsFetchRequested ({ value }) {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  }
+
+  onSuggestionsClearRequested () {
+    this.setState({
+      suggestions: []
+    });
+  }
+
+  render () {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: 'Search for players...',
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+      <Autosuggest 
+        suggestions={this.state.suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={this.getSuggestionValue}
+        renderSuggestion={this.renderSuggestion}
+        inputProps={inputProps} />
     );
   }
 }
