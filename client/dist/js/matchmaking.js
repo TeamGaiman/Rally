ad28d5425172b2c9a93a0c66339db9493ee4dcc2
frank <frankcorkery@gmail.com>
2018-08-29 10:49:25 -0400

    fixed matchmaking function to return larger dispersal of opponents

--- client/dist/js/matchmaking.js
+++ client/dist/js/matchmaking.js
@@ -8,14 +8,9 @@ const matchmakeByElo = ( eloToMatch, users ) => {
     return oddsBWillWin - oddsAWillWin;
   });
 
-  //slice first(hardest), last(easiest), then concat both with a filter
-  //to get players within 25 elo points of yourself
-  return sortedUserMatches.slice(0, 1).concat(
-    sortedUserMatches.slice(sortedUserMatches.length - 1),
-    sortedUserMatches.filter( ( person ) => {
-      return person.elo > eloToMatch - 25 && person.elo < eloToMatch + 25;
-    })
-  );
+  return sortedUserMatches.filter( ( person ) => {
+    return person.elo > eloToMatch - 125 && person.elo < eloToMatch + 125;
+  });
 
 };
 
