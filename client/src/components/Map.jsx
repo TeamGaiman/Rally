e4f74bb11f8fd53095fcb70352b558d8480b9624
parkermuir <parker.muir@gmail.com>
2018-08-30 22:39:38 -0400

    Resized map and modals

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -15,7 +15,7 @@ const Map = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: '100%' }} />,
-    containerElement: <div style={{ height: '425px' }} />,
+    containerElement: <div style={{ height: '365px' }} />,
     mapElement: <div style={{ height: '100%', width: '100%' }} />
   }),
   withStateHandlers(
